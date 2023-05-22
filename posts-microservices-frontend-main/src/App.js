a6c92f35102baf392882eb6390a9894fc3f80b97
vithor-vbs <vithorvilas12@gmail.com>
2023-05-22 17:17:35 -0300

    design and React logic refactor

--- posts-microservices-frontend-main/src/App.js
+++ posts-microservices-frontend-main/src/App.js
@@ -1,112 +1,212 @@
-import './App.css';
-import {useEffect, useState} from "react";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-    const [posts, setPosts] = useState([]);
-    let title, description, comment = '';
+  const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState("");
+  let comment = "";
+  const [description, setDescription] = useState("");
+  const [isVariableTrue, setIsVariableTrue] = useState(false);
 
-    useEffect(() => {
-        (async () => {
-            const response = await fetch('http://localhost:8000/api/posts');
+  
 
-            const content = await response.json()
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:8000/api/posts");
 
-            setPosts(content);
-        })()
-    }, []);
+      const content = await response.json();
 
-    const createPost = async e => {
-        e.preventDefault();
+      setPosts(content);
+    })();
+  }, []);
 
-        const res = await fetch('http://localhost:8000/api/posts', {
-            method: "POST",
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                title,
-                description,
-            })
-        });
+  const createPost = async (e) => {
+    e.preventDefault();
 
-        const createdPost = await res.json();
+    const existingPost = posts.find((post) => post.title === title);
+    if (existingPost) {
+      // Email already exists, update the description of the existing post
+      if (isVariableTrue) {
+        await deletePost(existingPost.id);
+        return;
+      }
 
-        setPosts([...posts, createdPost]);
+      const updatedPost = { ...existingPost, description };
+      const res = await fetch(
+        `http://localhost:8000/api/posts/${existingPost.id}`,
+        {
+          method: "PUT", 
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(updatedPost),
+        }
+      );
+
+      const updatedData = await res.json();
+      console.log("Post updated:", updatedData);
+
+      // Update the posts state with the modified post
+      setPosts(
+        posts.map((post) => (post.id === updatedData.id ? updatedData : post))
+      );
+    } else {
+      // Email does not exist, create a new post
+      const res = await fetch("http://localhost:8000/api/posts", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title,
+          description,
+        }),
+      });
+
+      const createdPost = await res.json();
+
+      setPosts([...posts, createdPost]);
     }
+  };
 
-    const createComment = async (e, post_id) => {
-        e.preventDefault();
+  const deletePost = async (postId) => {
+    try {
+      await fetch(`http://localhost:8000/api/posts/${postId}`, {
+        method: "DELETE",
+      });
 
-        const response = await fetch('http://localhost:8001/api/comments', {
-            method: "POST",
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                post_id,
-                text: comment
-            })
-        });
+      setIsVariableTrue(false);
 
-        const createdComment = await response.json();
-
-        comment = ''
-
-        setPosts(posts.map(p => {
-            if (p.id === post_id) {
-                if (!p.comments) {
-                    p.comments = []; 
-                  }
-                p.comments.push(createdComment)
-            }
-
-            return p;
-        }))
+      setPosts(posts.filter((post) => post.id !== postId));
+    } catch (error) {
+      console.log("Error deleting post:", error);
     }
-    return (
-        <div className="App container">
-            <form className="row headers" onSubmit={createPost}>
-                <div className="col-4">
-                    <h2>Cart Administrator</h2>
+  };
 
-                    <input className="form-control mb-3" placeholder="Enter your email" onChange={e => title = e.target.value}/>
-                    <textarea className="form-control mb-3" onChange={e => description = e.target.value}/>
+  const createComment = async (e, post_id) => {
+    e.preventDefault();
 
-                    <button className="btn btn-primary" type="submit">Save</button>
-                </div>
-            </form>
+    const response = await fetch("http://localhost:8001/api/comments", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        post_id,
+        text: comment,
+      }),
+    });
 
-            <main>
-                <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
-                    {posts.length && posts.map(
-                        post => {
-                            return (
-                                <div className="col" key={post.id}>
-                                    <div className="card mb-4 rounded-3 shadow-sm">
-                                        <div className="card-header py-3">
-                                            <h4 className="my-0 fw-normal">{post.title}</h4>
-                                        </div>
-                                        <div className="card-body">
-                                            <p className="card-title pricing-card-title">{post.description}</p>
-                                            <form onSubmit={e => createComment(e, post.id)}>
-                                                <input className="w-100 form-control"
-                                                       onChange={e => comment = e.target.value}/>
-                                            </form>
-                                        </div>
-                                        {post.comments && post.comments.map(
-                                            comment => {
-                                                return (
-                                                    <div className="card-footer py-3" key={comment.id}>
-                                                        {comment.text}
-                                                    </div>
-                                                )
-                                            }
-                                        )}
-                                    </div>
-                                </div>
-                            )
-                        }
-                    )}
-                </div>
-            </main>
-        </div>
+    const createdComment = await response.json();
+
+    comment = "";
+
+    setPosts(
+      posts.map((p) => {
+        if (p.id === post_id) {
+          if (!p.comments) {
+            p.comments = [];
+          }
+          p.comments.push(createdComment);
+        }
+
+        return p;
+      })
     );
+  };
+
+  const handleAssignButtonClick = () => {
+    setDescription("Subscription Purchased");
+  };
+  const handleCancelSubscription = () => {
+    setDescription("Subscription Cancelled");
+  };
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleClick = () => {
+    setIsVariableTrue(true);
+  };
+  return (
+    <div className="App container">
+      <form className="row headers-box" onSubmit={createPost}>
+        <div className="col-4">
+          <h2 className="header">Cart Administrator</h2>
+
+          <input
+            className="form-control mb-3 dark-color"
+            placeholder="Enter your email"
+            onChange={handleTitleChange}
+          />
+          <div className="button-box">
+            <button
+              className="btn btn-primary assign-button"
+              type="submit"
+              onClick={handleAssignButtonClick}
+            >
+              Assign
+            </button>
+            {posts.some((post) => post.title === title) && (
+              <button
+                className="btn btn-secundary"
+                onClick={handleCancelSubscription}
+              >
+                Cancel
+              </button>
+            )}
+            {posts.some((post) => post.title === title) && (
+              <button
+                className="btn btn-delete"
+                onClick={handleClick}
+              >
+                Remove User
+              </button>
+            )}
+          </div>
+        </div>
+      </form>
+
+      <main>
+        <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
+          {posts.length &&
+            posts.map((post) => {
+              return (
+                <div className="col" key={post.id}>
+                  <div className="card mb-4 rounded-3 shadow-sm darkest-color">
+                    <div className="card-header py-3">
+                      <h4 className="my-0 fw-normal">{post.title}</h4>
+                    </div>
+                    <div className="card-body">
+                      <p
+                        className={`card-title pricing-card-title ${
+                          post.description === "Subscription Cancelled"
+                            ? "cancelled-description"
+                            : ""
+                        } ${
+                          post.description === "Subscription Purchased"
+                            ? "purchased-description"
+                            : ""
+                        }`}
+                      >
+                        {post.description}
+                      </p>
+                      <form onSubmit={(e) => createComment(e, post.id)}>
+                        <input
+                          className="w-100 form-control dark-color"
+                          onChange={(e) => (comment = e.target.value)}
+                        />
+                      </form>
+                    </div>
+                    {post.comments &&
+                      post.comments.map((comment) => {
+                        return (
+                          <div className="card-footer py-3" key={comment.id}>
+                            {comment.text}
+                          </div>
+                        );
+                      })}
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- posts/core/views.py
+++ posts/core/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from .serializers import PostSerializer
 from .models import Post
 from rest_framework.response import Response
+from rest_framework import status
+
 
 class PostAPIView(APIView):
     def get(self, request):
@@ -14,3 +16,24 @@ class PostAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+            post = self.get_object(pk)
+            serializer = PostSerializer(post, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+    
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise status.HTTP_404_NOT_FOUND
+        
+    def delete(self, request, pk):
+        try:
+            post = Post.objects.get(id=pk)
+            post.delete()
+            return Response({'message': 'Post deleted successfully.'})
+        except Post.DoesNotExist:
+            return Response({'message': 'Post not found.'})
